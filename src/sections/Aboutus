6ab1c5552801be84323096d8355c07f13dee1801
favy-codez <107569845+favy-codez@users.noreply.github.com>
2023-10-26 18:58:47 +0100

    added information page

---
+++ src/sections/Aboutus
@@ -0,0 +1,25 @@
+import React from "react";
+import chef5 from '../assets/chef5.jfif'
+import {Button} from '../components/Button.jsx'
+
+const Aboutus = () => {
+    return (
+      <div>
+        <div className="aboutUsWrapper flex px-[90px] py-[60px] bg-gradient-to-r from-[#fef5f0] to-[#d1cfd0]/30">
+            <div className="img w-1/2 pr-[120px]">
+              <img src={chef5} alt="" className="w-[40%] float-right" />
+            </div>
+            <div className="aboutUs w-1/2">
+              <h2 className="font-bold text-xl">About Us <br/> We Have Provided A Quality Driver</h2>
+              <p className="py-[30px]">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Blanditiis molestiae alias adipisci quam mollitia corporis 
+                maiores ducimus dolore 
+                consequuntur quod. Veniam nam velit accusantium nemo? Nisi similique numquam placeat? Earum.</p>
+                <Button text={"READ MORE"}></Button>
+            </div>
+        </div>
+      </div>
+    );
+  };
+  
+  export default Aboutus;
+  
---
+++ src/sections/Blog.jsx
@@ -0,0 +1,52 @@
+// import React from "react";
+import img1 from '../assets/img1.jfif';
+import img2 from '../assets/img2.jfif';
+import img3 from '../assets/img3.jfif';
+const Blog = () => {
+    return (
+      <div>
+        <div className="blogWrapper mb-[70px]">
+          <div className="blogHead text-center my-[70px]">
+            <h2 className="font-bold text-2xl">Blog Our Latest News</h2>
+            <p className="w-[600px] mx-auto ">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur doloremque voluptas 
+              repudiandae quos numquam vero. Doloribus, reiciendis? Voluptates, </p>
+          </div>
+          <div className="blogCardCon flex justify-center gap-[30px] ">
+            <div className="blogCard w-[450px] pb-[25px] rounded-xl shadow">
+              <div className="imgCard bg-[#f8f9fe] py-[35px] rounded-t-xl">
+                <img src={img1} alt="" className="rounded-[100%] w-[280px] h-[280px] mx-auto"/>
+              </div>
+              <div className="cardDetails px-[50px]">
+                <h4 className="font-semibold pt-[15px]">Lorem ipsum dolor, sit amet consectetur.</h4>
+                <p className="py-[20px]">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa totam, voluptas exercitationem repellat dolore unde, nesciunt error, doloremque beatae reiciendis veniam nisi ullam inventore sint. Laborum ad illo suscipit accusantium.</p>
+                <a href="" className="text-[#ff7518] mt-[10px]">Read More</a>
+              </div>
+            </div>
+            <div className="blogCard w-[450px] pb-[25px] rounded-xl shadow">
+              <div className="imgCard bg-[#f8f9fe] py-[35px] rounded-t-xl">
+                <img src={img2} alt="" className="rounded-[100%] w-[280px] h-[280px] mx-auto"/>
+              </div>
+              <div className="cardDetails px-[50px]">
+                <h4 className="font-semibold pt-[15px]">Lorem ipsum dolor, sit amet consectetur.</h4>
+                <p className="py-[20px]">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa totam, voluptas exercitationem repellat dolore unde, nesciunt error, doloremque beatae reiciendis veniam nisi ullam inventore sint. Laborum ad illo suscipit accusantium.</p>
+                <a href="" className="text-[#ff7518] mt-[10px]">Read More</a>
+              </div>
+            </div>
+            <div className="blogCard w-[450px] pb-[25px] rounded-xl shadow">
+              <div className="imgCard bg-[#f8f9fe] py-[35px] rounded-t-xl">
+                <img src={img3} alt="" className="rounded-[100%] w-[280px] h-[280px] mx-auto"/>
+              </div>
+              <div className="cardDetails px-[50px]">
+                <h4 className="font-semibold pt-[15px]">Lorem ipsum dolor, sit amet consectetur.</h4>
+                <p className="py-[20px]">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa totam, voluptas exercitationem repellat dolore unde, nesciunt error, doloremque beatae reiciendis veniam nisi ullam inventore sint. Laborum ad illo suscipit accusantium.</p>
+                <a href="" className="text-[#ff7518] mt-[10px]">Read More</a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  };
+  
+  export default Blog;
+  
